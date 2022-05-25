9114cccb0d0b0aff414431ad575324b17c0a88d0
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-25 13:21:55 +0430

    Implement connectDB

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const app = express()
+const connectDB = require('./db/connection')
 
 // middleware
 app.use(express.json())
@@ -9,6 +10,13 @@ app.use(express.json())
 
 module.exports = async (port) => {
     try {
+        // connection to the database
+        const {
+            MONGO_URL,
+            MONGO_DB_NAME
+        } = process.env
+        await connectDB(MONGO_URL, MONGO_DB_NAME)
+
         app.listen(port, () => {
             console.log(`The server is listening on port: ${port}`);
         })
